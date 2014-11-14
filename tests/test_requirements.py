f407885fc22d00752aa1bafd0c0874ca3a2da1b8
Dennis Ploeger <develop@dieploegers.de>
2014-11-14 16:58:37 +0100

    * Further tests

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -37,21 +37,17 @@ class RequirementsTestCase(TestCase):
 
         pass
 
-    def test_wrong_ip(self):
-
-        """ A Requirement requiring a specific IP should work when connecting from the right ip and fail otherwise
-        """
+    def tool_basic_requirement(self):
 
         requirement = models.Requirement()
 
         requirement.rule = self.rule
 
-        requirement.sender_ip = "1.1.1.1"
-        requirement.sender_ip_cidr = "32"
-
         requirement.action = constants.REQ_ACTION_ACCEPT
 
-        requirement.save()
+        return requirement
+
+    def tool_get_helper(self):
 
         configuration = build_configuration()
 
@@ -61,6 +57,22 @@ class RequirementsTestCase(TestCase):
 
         self.assertTrue(helper.enabled, "Helper wasn't enabled after initialization.")
 
+        return helper
+
+    def test_wrong_ip(self):
+
+        """ A requirement requiring a specific IP should work when connecting from the right ip and fail otherwise
+        """
+
+        requirement = self.tool_basic_requirement()
+
+        requirement.sender_ip = "1.1.1.1"
+        requirement.sender_ip_cidr = "32"
+
+        requirement.save()
+
+        helper = self.tool_get_helper()
+
         # Try to mimic a connection from the right IP
 
         helper.connect("", "", "1.1.1.1", "", {})
@@ -69,8 +81,63 @@ class RequirementsTestCase(TestCase):
 
         # Try to mimic a connection from the wrong IP
 
-        helper = MilterHelper(configuration)
+        helper = self.tool_get_helper()
 
         helper.connect("", "", "1.1.1.2", "", {})
 
         self.assertFalse(helper.enabled, "Helper was enabled after connecting with the wrong IP")
+
+    def test_wrong_sender(self):
+
+        """ A requirement requiring a specific sender should work when using it and fail otherwise
+        """
+
+        requirement = self.tool_basic_requirement()
+
+        requirement.sender = "abc"
+
+        requirement.save()
+
+        helper = self.tool_get_helper()
+
+        # Try to mimic a valid sender
+
+        helper.connect("", "", "1.1.1.1", "", {})
+        helper.mail_from("abc", {})
+
+        self.assertTrue(helper.enabled, "Helper wasn't enabled after sending to the right sender")
+
+        helper = self.tool_get_helper()
+
+        helper.connect("", "", "1.1.1.1", "", {})
+        helper.mail_from("def", {})
+
+        self.assertFalse(helper.enabled, "Helper was enabled after sending to the wrong sender")
+
+
+    def test_wrong_recipient(self):
+
+        """ A requirement requiring a specific recipient should work when using it and fail otherwise
+        """
+
+        requirement = self.tool_basic_requirement()
+
+        requirement.recipient = "abc"
+
+        requirement.save()
+
+        helper = self.tool_get_helper()
+
+        # Try to mimic a valid recipient
+
+        helper.connect("", "", "1.1.1.1", "", {})
+        helper.rcpt("abc", {})
+
+        self.assertTrue(helper.enabled, "Helper wasn't enabled after sending to the right recipient")
+
+        helper = self.tool_get_helper()
+
+        helper.connect("", "", "1.1.1.1", "", {})
+        helper.rcpt("def", {})
+
+        self.assertFalse(helper.enabled, "Helper was enabled after sending to the wrong recipient")
