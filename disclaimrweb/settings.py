a589307dd669a16f3c5137decdd410a0effa494a
Dennis Ploeger <develop@dieploegers.de>
2014-11-14 09:55:38 +0100

    * Added default configuration files * Added secret key generator

--- disclaimrweb/settings.py
+++ disclaimrweb/settings.py
@@ -77,7 +77,27 @@ GRAPPELLI_ADMIN_TITLE = "DisclaimR Web Administration"
 
 # Import local settings
 
-conffiles = glob.glob(os.path.join(os.path.dirname(__file__), 'settings', '*.conf'))
+conffiles = glob.glob(os.path.join(BASE_DIR, 'settings', '*.conf'))
 conffiles.sort()
 for f in conffiles:
     execfile(os.path.abspath(f))
+
+# Generate a SECRET_KEY if it hasn't been already set. Taken from https://gist.github.com/ndarville/3452907
+
+try:
+    SECRET_KEY
+except NameError:
+    SECRET_FILE = os.path.join(BASE_DIR, 'secret.txt')
+    try:
+        SECRET_KEY = open(SECRET_FILE).read().strip()
+    except IOError:
+        try:
+            import random
+            SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in
+                                  range(50)])
+            secret = file(SECRET_FILE, 'w')
+            secret.write(SECRET_KEY)
+            secret.close()
+        except IOError:
+            Exception('Please create a %s file with random characters \
+            to generate your secret key!' % SECRET_FILE)
